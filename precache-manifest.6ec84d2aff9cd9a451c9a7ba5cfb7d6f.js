b8490ea7b97375afe8cdd7579150fd23a6a1f01d
muhdarfan <46324706+muhdarfan@users.noreply.github.com>
2019-12-16 22:56:35 +0800

    Updates

---
+++ precache-manifest.6ec84d2aff9cd9a451c9a7ba5cfb7d6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "253d591c519365b69d7df045faed2b89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d29e69cfae98ffcaf27",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6962b1f9b81f913fcff7",
+    "url": "/static/js/2.56347bbf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.56347bbf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d29e69cfae98ffcaf27",
+    "url": "/static/js/main.409bd0d2.chunk.js"
+  },
+  {
+    "revision": "197e7078eb4989db7b68",
+    "url": "/static/js/runtime-main.3e9d2655.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
